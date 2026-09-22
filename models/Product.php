eb8c00c6ddb12bc1aaf514b1c8fc62d6d07925f6
heius-06 <hieu1357n@gmail.com>
2026-09-22 04:57:23 -0700

    Update Product.php

--- models/Product.php
+++ models/Product.php
@@ -2,36 +2,25 @@
 class Product {
     private $conn;
     private $table_name = "products";
+    public function __construct($db) { $this->conn = $db; }
 
-    public function __construct($db) {
-        $this->conn = $db;
-    }
-
-    // Lấy dữ liệu động dựa trên bộ lọc
     public function getFiltered($keyword = "", $brand = "") {
         $query = "SELECT * FROM " . $this->table_name . " WHERE 1=1";
-        
-        if (!empty($keyword)) {
-            $query .= " AND name LIKE :keyword";
-        }
-        if (!empty($brand)) {
-            $query .= " AND brand = :brand";
-        }
-        
-        $query .= " ORDER BY id ASC";
-        
+        if (!empty($keyword)) $query .= " AND name LIKE :keyword";
+        if (!empty($brand)) $query .= " AND brand = :brand";
         $stmt = $this->conn->prepare($query);
-        
-        if (!empty($keyword)) {
-            $keyword_param = "%{$keyword}%";
-            $stmt->bindParam(":keyword", $keyword_param);
-        }
-        if (!empty($brand)) {
-            $stmt->bindParam(":brand", $brand);
-        }
-        
+        if (!empty($keyword)) { $kw = "%{$keyword}%"; $stmt->bindParam(":keyword", $kw); }
+        if (!empty($brand)) $stmt->bindParam(":brand", $brand);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getById($id) {
+        $query = "SELECT * FROM " . $this->table_name . " WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 }
 ?>
