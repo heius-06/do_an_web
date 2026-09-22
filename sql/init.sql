3dbabde193ef9732897361f3f30fbfc7e0ede3d1
heius-06 <hieu1357n@gmail.com>
2026-09-22 05:16:29 -0700

    Update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -6,7 +6,7 @@ CREATE TABLE products (
     brand VARCHAR(50) NOT NULL,
     price INT NOT NULL,
     old_price INT,
-    discount_label VARCHAR(50),
+    discount_label VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
     image_url TEXT NOT NULL,
     ram VARCHAR(20),
     rom VARCHAR(20),
@@ -19,6 +19,6 @@ INSERT INTO products (name, brand, price, old_price, discount_label, image_url, 
 ('iPhone 15', 'Apple', 19990000, 22990000, 'Hot Sale', 'https://cdn.tgdd.vn/Products/Images/42/281570/iphone-15-hong-thumb-600x600.jpg', '6 GB', '128 GB', 4.8),
 ('Galaxy S24 Ultra', 'Samsung', 33990000, 37990000, 'Tặng 2 triệu', 'https://cdn.tgdd.vn/Products/Images/42/307174/samsung-galaxy-s24-ultra-grey-thumb-600x600.jpg', '12 GB', '256 GB', 4.8),
 ('Galaxy Z Fold5', 'Samsung', 40990000, 44990000, 'Giảm sốc 4Tr', 'https://cdn.tgdd.vn/Products/Images/42/301608/samsung-galaxy-z-fold5-blue-thumbnew-600x600.jpg', '12 GB', '512 GB', 4.9),
-('Xiaomi 14', 'Xiaomi', 19990000, 22990000, 'Bảo hành 24T', 'https://cdn.tgdd.vn/Products/Images/42/319672/xiaomi-14-black-thumbnew-600x600.jpg', '12 GB', '256 GB', 4.5),
-('OPPO Find N3', 'OPPO', 44990000, 44990000, 'Pre-order', 'https://cdn.tgdd.vn/Products/Images/42/317804/oppo-find-n3-gold-thumbnew-600x600.jpg', '16 GB', '512 GB', 4.9),
-('OPPO Reno11 F', 'OPPO', 8990000, 8990000, 'Hot', 'https://cdn.tgdd.vn/Products/Images/42/321873/oppo-reno11-f-xanh-thumb-600x600.jpg', '8 GB', '256 GB', 4.5);
+('Xiaomi Redmi Note 13', 'Xiaomi', 4890000, 5290000, 'Bảo hành 24T', 'https://cdn.tgdd.vn/Products/Images/42/309831/xiaomi-redmi-note-13-gold-thumb-600x600.jpg', '8 GB', '128 GB', 4.5),
+('OPPO Find N2 Flip', 'OPPO', 16990000, 19990000, 'Giảm 3 Triệu', 'https://cdn.tgdd.vn/Products/Images/42/300121/oppo-find-n2-flip-tim-thumb-600x600.jpg', '8 GB', '256 GB', 4.9),
+('OPPO Reno10 5G', 'OPPO', 9490000, 9990000, 'Mới ra mắt', 'https://cdn.tgdd.vn/Products/Images/42/304481/oppo-reno10-blue-thumbnew-600x600.jpg', '8 GB', '256 GB', 4.5);
