f4c6dfe48d48954d5763c70f471cba2e5137c655
heius-06 <hieu1357n@gmail.com>
2026-09-22 04:06:25 -0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -11,6 +11,8 @@ class Database {
         try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8mb4", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // Ép buộc sử dụng UTF-8 để không bị lỗi font Tiếng Việt
+            $this->conn->exec("SET NAMES 'utf8mb4'");
         } catch(PDOException $exception) {
             echo "Lỗi kết nối CSDL: " . $exception->getMessage();
         }
